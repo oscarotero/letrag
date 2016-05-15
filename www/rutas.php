7be7662720d2d3db8e9f00f155e0bc55d6da5021
root <root@oscarotero.com>
2016-05-15 15:16:58 -0400

    non mostrar erros estrictos

--- www/rutas.php
+++ www/rutas.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('error_reporting', E_ALL);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ini_set('expose_php', 0);
